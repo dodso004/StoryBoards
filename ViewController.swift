a14eb89d075dd9fe58a617aec92e6e7225932b02
dodso004 <61028392+dodso004@users.noreply.github.com>
2020-05-07 17:02:07 -0500

    Video #1: New Project + IB (Interface Builder) tour

---
+++ ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PlayersRatings
+//
+//  Created by Alex Dodson on 5/7/20.
+//  Copyright © 2020 Adam Dodson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
